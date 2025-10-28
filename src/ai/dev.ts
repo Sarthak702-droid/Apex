368a3d773d2af0a19f62d0ddef798e012510cecb
Sarthak Tripathy <sarthaktripathy2004@gmail.com>
2025-10-28 05:39:32 +0000

    this error is comming

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/buyer-dashboard-ai-quality-reports.ts';
 import '@/ai/flows/farmer-dashboard-crop-recommendations.ts';
 import '@/ai/flows/conversational-agent.ts';
